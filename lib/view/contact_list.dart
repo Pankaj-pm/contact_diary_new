2eb25decf04d54860e4c5501b78710c0056e941f
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-05-03 09:25:14 +0530

    multi provider , shared pref

--- lib/view/contact_list.dart
+++ lib/view/contact_list.dart
@@ -4,6 +4,7 @@ import 'package:contact_diary/view/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
@@ -24,36 +25,55 @@ class _ContactPageState extends State<ContactPage> {
               ContactModel contact = value.contactList[index];
 
               return ListTile(
-                leading: CircleAvatar(child: Text("${contact.name ?? ""}"[0].toUpperCase())),
+                leading: InkWell(
+                  onTap: () {
+                    launchUrl(Uri.parse("tel:+1-555-010-999"));
+                  },
+                  child: CircleAvatar(
+                    child: Text("${contact.name ?? ""}"[0].toUpperCase()),
+                  ),
+                ),
                 title: Text(contact.name ?? ""),
                 subtitle: Text(contact.number ?? ""),
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return DetailPage(index: index,);
-                          },
-                        ));
-                      },
-                      icon: Icon(Icons.edit),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        Provider.of<ContactProvider>(context, listen: false).deleteContact(index);
-                      },
-                      icon: Icon(Icons.delete),
-                    ),
-                    IconButton(
-                      onPressed: () {
 
-                        String con='Please Use this number ${contact.name ?? ""},${contact.number ?? ""} ';
-                        Share.share(con);
+
+
+                    PopupMenuButton(
+                      child: Icon(Icons.add),
+                      tooltip: "",
+                      itemBuilder: (context) {
+                        return [
+                          PopupMenuItem(
+                            child: Text("Delete"),
+                            onTap: () {
+                              Provider.of<ContactProvider>(context, listen: false).deleteContact(index);
+                            },
+                          ),
+                          PopupMenuItem(
+                            child: Text("Share"),
+                            onTap: () {
+                              String con = 'Please Use this number ${contact.name ?? ""},${contact.number ?? ""} ';
+                              Share.share(con);
+                            },
+                          ),
+                          PopupMenuItem(
+                            child: Text("Edit"),
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(
+                                builder: (context) {
+                                  return DetailPage(
+                                    index: index,
+                                  );
+                                },
+                              ));
+                            },
+                          ),
+                        ];
                       },
-                      icon: Icon(Icons.share),
-                    ),
+                    )
                   ],
                 ),
               );
